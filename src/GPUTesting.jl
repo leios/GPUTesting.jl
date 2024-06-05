08289cd33128ee7609a3439ca144954c31c342d8
Maxwell Onyango <maxmlewa@mit.edu>
2024-06-05 10:32:05 -0400

    added attempt at matrix multiplication
    Co-authored-by: Maxwell Onyango <maxmlewa@mit.edu>

--- src/GPUTesting.jl
+++ src/GPUTesting.jl
@@ -4,5 +4,6 @@ using KernelAbstractions
 
 include("v_add.jl")
 include("v_mult.jl")
+include("matrix_mult.jl")
 
 end # module GPUTesting
--- test/runtests.jl
+++ test/runtests.jl
@@ -10,7 +10,15 @@ using GPUTesting
     v_add!(a, b, c)
     @test a .+ b == c
 
-    #vector multiplication tests
+    # vector multiplication tests
     v_mult!(a, b, c)
     @test a .* b == c
+
+    X = rand(10,20)
+    Y = rand(20,30)
+    Z = rand(10,30)
+
+    # matrix multiplication tests
+    matrix_mult!(X,Y,Z)
+    @test isapprox(Z, X*Y)
 end
