7c4ef97ad3ccc5c73858adf08dfa31d0fcb304e8
Maxwell Onyango <maxmlewa@mit.edu>
2024-06-10 16:33:45 -0400

    Added generic test for matarix multiplication

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,6 +6,10 @@ using GPUTesting
     b = rand(100)
     c = similar(a)
 
+    X = rand(10,20)
+    Y = rand(20,30)
+    Z = rand(10,30)
+
     # vector addition tests
     v_add!(a, b, c)
     @test a .+ b == c
@@ -13,4 +17,8 @@ using GPUTesting
     #vector multiplication tests
     v_mult!(a, b, c)
     @test a .* b == c
+
+    #matrix multiplication tests
+    matrix_mult!(X,Y,Z)
+    @test isapprox(Z, X*Y)
 end
