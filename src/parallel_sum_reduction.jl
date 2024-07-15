08ad089419ca00eebd38df8e77b79930137d90aa
Maxwell Onyango <maxmlewa@mit.edu>
2024-07-15 08:42:56 -0400

    parallel sum reduction, only group works

--- src/parallel_sum_reduction.jl
+++ src/parallel_sum_reduction.jl
@@ -14,7 +14,7 @@ export sumGPU!
 
     @uniform input_length = length(input_array)
 
-    # private variable for tile output
+    # private variable for tile output : DEBUGGING
     outval = @private eltype(input_array) 1
     @inbounds outval[1] = -zero(eltype(input_array))
 
@@ -24,24 +24,41 @@ export sumGPU!
     #set the tile elements to 0
     @inbounds tile[i] = 0.0
 
-    @inbounds tile[i] = input_array[gi]
     @synchronize
 
 
+    I = (group_id-1)*group_size+i
+    if I <= input_length
+        @inbounds tile[i] = input_array[I]
+    else
+        tile[i] = 0.0
+    end 
+    @synchronize
 
+
+    # The incorrect part
     for k in 1:end_point
         @synchronize
+
         STRIDE = div(group_size, 2^k)
-        
+        I = (group_id-1)*group_size+i
         if (i <= STRIDE)
             @inbounds tile[i] += tile[i+STRIDE]  
-        end         
+        end
     end
+    # The incorrect part : saving the first element to the partial sums 
 
     
+    # modifying input array for debugging ###
+    I = (group_id-1)*group_size+i
     
+    if I <= input_length
+        @inbounds input_array[I] = tile[i]
+    end
+    @synchronize
+    
+
     partial_sum[group_id] = tile[1]
-   
 end
 
 function sumGPU!(a; nthreads = 256)
