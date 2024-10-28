be18bcd26b6ad3477193ad7218271226f5ad7f99
Maxwell Onyango <maxmlewa@mit.edu>
2024-10-28 00:20:48 -0400

    Removed extra spacing, CI tests

--- src/performant_TRMM.jl
+++ src/performant_TRMM.jl
@@ -92,8 +92,8 @@ function recursive_TRMM!(A_2, B_2, size_a, LIMIT = 16)
         # recursive case
         h_size = div(size_a, 2)
         #step 1
-        recursive_TRMM!(A_2[h_size + 1: end ,h_size +1: end], @view(B_2[h_size + 1: end ,1:h_size]), h_size, LIMIT)
-        recursive_TRMM!(A_2[h_size + 1: end ,h_size +1: end], @view(B_2[h_size + 1: end ,h_size + 1: end]), h_size, LIMIT)
+        recursive_TRMM!(A_2[h_size+1: end ,h_size+1: end], @view(B_2[h_size+1: end ,1:h_size]), h_size, LIMIT)
+        recursive_TRMM!(A_2[h_size+1: end ,h_size+1: end], @view(B_2[h_size+1: end ,h_size+1:end]), h_size, LIMIT)
 
         #step 2: GEMM
         B_2[h_size + 1: end ,1:h_size]        =  B_2[h_size + 1: end ,1:h_size] + (A_2[h_size + 1: end , 1: h_size] * B_2[1:h_size, 1:h_size])
