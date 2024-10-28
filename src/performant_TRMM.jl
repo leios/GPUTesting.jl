47dfd5fd7d4e763ca96f15f199fd20c884cecc3f
Maxwell Onyango <maxmlewa@mit.edu>
2024-10-28 07:16:20 -0400

    Removed extra spacing, CI tests

--- src/performant_TRMM.jl
+++ src/performant_TRMM.jl
@@ -10,7 +10,7 @@ end
 
 
 
-function performant_TRMM!(A, B, LIMIT = 16 ; n_threads = (16,16) )
+function performant_TRMM!(A, B, LIMIT = 16 ; n_threads = (16,16))
 
     #resize
      
@@ -77,27 +77,27 @@ function recursive_TRMM!(A_2, B_2, size_a, LIMIT = 16)
 
     if (size_a < LIMIT)
         #step 1 TRMM
-        B_2[size_a + 1: end , 1:size_a]        = A_2[size_a + 1: end ,size_a+1: end] * B_2[size_a + 1: end ,1:size_a]
-        B_2[size_a + 1: end , size_a + 1: end] = A_2[size_a + 1: end ,size_a+1: end] * B_2[size_a + 1: end ,size_a + 1: end]
+        B_2[size_a+1:end , 1:size_a]        = A_2[size_a+1:end ,size_a+1: end] * B_2[size_a+1:end, 1:size_a]
+        B_2[size_a+1:end , size_a+1:end] = A_2[size_a+1:end ,size_a+1: end] * B_2[size_a+1:end, size_a+1:end]
 
         #step 2
-        B_2[size_a + 1: end ,1:size_a]        =  B_2[size_a + 1: end ,1:size_a] + (A_2[size_a + 1: end , 1: size_a] * B_2[1:size_a ,1:size_a])
-        B_2[size_a + 1: end ,size_a + 1: end] =   B_2[size_a + 1: end ,size_a + 1: end] + (A_2[size_a + 1: end , 1: size_a] * B_2[1:size_a ,size_a + 1: end])
+        B_2[size_a+1:end, 1:size_a]        =  B_2[size_a+1:end ,1:size_a] + (A_2[size_a+1:end , 1:size_a] * B_2[1:size_a,1:size_a])
+        B_2[size_a+1:end, size_a+1:end] =   B_2[size_a+1:end ,size_a+1:end] + (A_2[size_a+1:end , 1:size_a] * B_2[1:size_a, size_a+1:end])
 
         #step 3
-        B_2[1 : size_a, 1: size_a] = A_2[ 1: size_a , 1: size_a  ] * B_2[1: size_a , 1: size_a ]
-        B_2[1 : size_a , size_a + 1: end] = A_2[ 1: size_a , 1: size_a  ] * B_2[1: size_a , size_a + 1: end]
+        B_2[1:size_a, 1:size_a] = A_2[ 1:size_a , 1:size_a] * B_2[1:size_a , 1:size_a]
+        B_2[1:size_a , size_a+1:end] = A_2[1:size_a , 1:size_a] * B_2[1:size_a , size_a+1:end]
 
     else
         # recursive case
         h_size = div(size_a, 2)
         #step 1
-        recursive_TRMM!(A_2[h_size+1: end ,h_size+1: end], @view(B_2[h_size+1: end ,1:h_size]), h_size, LIMIT)
-        recursive_TRMM!(A_2[h_size+1: end ,h_size+1: end], @view(B_2[h_size+1: end ,h_size+1:end]), h_size, LIMIT)
+        recursive_TRMM!(A_2[h_size+1:end, h_size+1:end], @view(B_2[h_size+1:end ,1:h_size]), h_size, LIMIT)
+        recursive_TRMM!(A_2[h_size+1:end, h_size+1:end], @view(B_2[h_size+1:end ,h_size+1:end]), h_size, LIMIT)
 
         #step 2: GEMM
-        B_2[h_size + 1: end ,1:h_size]        =  B_2[h_size + 1: end ,1:h_size] + (A_2[h_size + 1: end , 1: h_size] * B_2[1:h_size, 1:h_size])
-        B_2[h_size + 1: end ,h_size + 1: end] =   B_2[h_size + 1: end ,h_size + 1: end] + (A_2[h_size + 1: end , 1: h_size] * B_2[1:h_size ,h_size + 1: end])
+        B_2[h_size+1:end ,1:h_size]        =  B_2[h_size + 1: end ,1:h_size] + (A_2[h_size + 1: end , 1: h_size] * B_2[1:h_size, 1:h_size])
+        B_2[h_size+1:end ,h_size+1:end] =   B_2[h_size + 1: end ,h_size + 1: end] + (A_2[h_size + 1: end , 1: h_size] * B_2[1:h_size ,h_size + 1: end])
 
 
         #step 3
